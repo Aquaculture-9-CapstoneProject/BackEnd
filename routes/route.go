fcac95f588c98347084a31a8e5e45058a90a5f51
MichaelSitanggang <michaelsitanggang37@gmail.com>
2024-11-27 21:53:42 +0700

    tambah cors

--- routes/route.go
+++ routes/route.go
@@ -1,13 +1,27 @@
 package routes
 
 import (
+	"time"
+
 	"github.com/Aquaculture-9-CapstoneProject/BackEnd.git/controllers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Routes(authControl *controllers.AuthCotroller) *gin.Engine {
 	r := gin.Default()
 
+	// Tambahkan middleware CORS
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	// Rute untuk register dan login
 	r.POST("/register", authControl.DaftarAkun)
 	r.POST("/login", authControl.Login)
 
