6236069c367c937a3c887b6007861f08171def77
MichaelSitanggang <michaelsitanggang37@gmail.com>
2024-11-27 18:22:01 +0700

    sedikit tambahan

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/Aquaculture-9-CapstoneProject/BackEnd.git/repositories"
 	"github.com/Aquaculture-9-CapstoneProject/BackEnd.git/routes"
 	"github.com/Aquaculture-9-CapstoneProject/BackEnd.git/services"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -13,6 +15,18 @@ func main() {
 	authRepo := repositories.NewAuthRepo(db)
 	authServis := services.NewAuthUseCase(authRepo)
 	authControl := controllers.NewAuthController(authServis)
-	r := routes.Routes(authControl)
+
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+
+	routes.Routes(authControl)
+
 	r.Run(":8000")
 }
