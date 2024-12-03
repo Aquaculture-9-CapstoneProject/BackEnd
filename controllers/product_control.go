d43e531e5d1574e339ee8c064a57b3804243c03d
MichaelSitanggang <michaelsitanggang37@gmail.com>
2024-12-03 13:55:57 +0700

    Homepage done

--- controllers/product_control.go
+++ controllers/product_control.go
@@ -20,18 +20,18 @@ func (ctrl *ProductIkanController) GetTermurahProduk(c *gin.Context) {
 	limit, _ := strconv.Atoi(c.DefaultQuery("limit", "5"))
 	produk, err := ctrl.service.GetProdukTermurah(limit)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"Message": err.Error()})
+		c.JSON(http.StatusInternalServerError, gin.H{"meta": gin.H{"message": err.Error(), "code": 401, "status": "eror"}, "data": ""})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"Meta": gin.H{"Message": "Berhasil", "Code": 200, "Status": "Berhasil"}, "Data": produk})
+	c.JSON(http.StatusOK, gin.H{"meta": gin.H{"message": "Berhasil", "code": 200, "status": "Berhasil"}, "data": produk})
 }
 
 func (ctrl *ProductIkanController) GetPopulerProduk(c *gin.Context) {
 	limit, _ := strconv.Atoi(c.DefaultQuery("limit", "4"))
 	produk, err := ctrl.service.GetAllProductPopuler(limit)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"Meta": gin.H{"Message": err.Error(), "Code": 401, "Status": "eror"}, "Data": ""})
+		c.JSON(http.StatusInternalServerError, gin.H{"meta": gin.H{"message": err.Error(), "code": 401, "status": "eror"}, "data": ""})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"Meta": gin.H{"Message": "Berhasil", "Code": 200, "Status": "Berhasil"}, "Data": produk})
+	c.JSON(http.StatusOK, gin.H{"meta": gin.H{"message": "Berhasil", "code": 200, "status": "Berhasil"}, "data": produk})
 }
