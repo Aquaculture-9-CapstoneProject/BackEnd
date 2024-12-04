635d36cc2a55dd6f2e6573384908907cf183b248
Aldian Eka K <aldianeka88@gmail.com>
2024-12-04 22:08:19 +0700

    Create CRUD for artikel

--- config/database.go
+++ config/database.go
@@ -29,7 +29,7 @@ func CreateDatabase() *gorm.DB {
 	if err != nil {
 		panic(err)
 	}
-	db.AutoMigrate(entities.User{}, entities.Admin{}, entities.Product{})
+	db.AutoMigrate(entities.User{}, entities.Admin{}, entities.Product{}, entities.Artikel{})
 
 	return db
 }
