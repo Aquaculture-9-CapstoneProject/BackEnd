bba801973624e4ade39b6db12ccdb6aaa1a6031c
MichaelSitanggang <michaelsitanggang37@gmail.com>
2024-12-11 20:32:42 +0700

    tambahan

--- entities/product.go
+++ entities/product.go
@@ -1,18 +1,19 @@
 package entities
 
 type Product struct {
-	ID           int
-	Gambar       string
-	Nama         string
-	Deskripsi    string
-	Keunggulan   string
-	Harga        int
-	Variasi      string
-	Kategori     string
-	KotaAsal     string
-	Rating       float64
-	Stok         int
-	TotalReview  int
-	Reviews      []Review      `gorm:"foreignKey:ProductID" json:"reviews"`
-	OrderDetails []OrderDetail `gorm:"foreignKey:ProductID" json:"order_details,omitempty"`
+	ID              int
+	Gambar          string
+	Nama            string
+	Deskripsi       string
+	Keunggulan      string
+	Harga           int
+	Variasi         string
+	Kategori        string
+	KotaAsal        string
+	Rating          float64
+	Stok            int
+	TotalReview     int
+	TipsPenyimpanan string
+	Reviews         []Review      `gorm:"foreignKey:ProductID" json:"reviews"`
+	OrderDetails    []OrderDetail `gorm:"foreignKey:ProductID" json:"order_details,omitempty"`
 }
