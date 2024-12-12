036bd7161d673e4d4f5bf3d87125e5ce4b766f9b
MichaelSitanggang <michaelsitanggang37@gmail.com>
2024-12-13 00:33:34 +0700

    push

--- entities/Payment.go
+++ entities/Payment.go
@@ -2,7 +2,7 @@ package entities
 
 type Payment struct {
 	ID           int
-	InvoiceID    string `gorm:"uniqueIndex"`
+	InvoiceID    string `gorm:"type:longtext;uniqueIndex:idx_invoice_id(255)"`
 	Status       string
 	StatusBarang string
 	Jumlah       float64
