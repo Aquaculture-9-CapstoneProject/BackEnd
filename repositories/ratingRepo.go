106dda8c23f1b78a3cb869ec884cd0108e175b97
MichaelSitanggang <michaelsitanggang37@gmail.com>
2024-12-12 12:44:47 +0700

    push

--- repositories/ratingRepo.go
+++ repositories/ratingRepo.go
@@ -1,16 +1,1 @@
 package repositories
-
-import (
-	"gorm.io/gorm"
-)
-
-type RatingRepo interface {
-}
-
-type ratingRepo struct {
-	db *gorm.DB
-}
-
-func NewRatingRepo(db *gorm.DB) RatingRepo {
-	return &ratingRepo{db: db}
-}
