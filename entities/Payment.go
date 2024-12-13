002c791345cf3d1b9618fb9ce29689411b45ec76
MichaelSitanggang <michaelsitanggang37@gmail.com>
2024-12-13 17:47:10 +0700

    push debug

--- entities/Payment.go
+++ entities/Payment.go
@@ -4,7 +4,6 @@ type Payment struct {
 	ID           int    `gorm:"primaryKey"`
 	InvoiceID    string `gorm:"type:varchar(255);uniqueIndex"`
 	Status       string
-	UserID       int
 	StatusBarang string
 	Jumlah       float64
 	OrderID      int   `gorm:"not null"`
