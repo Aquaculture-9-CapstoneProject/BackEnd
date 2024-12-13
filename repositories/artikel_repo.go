d7ab10ee2b975cdcdbe70522190a10d4e9e1eedc
Aldian Eka K <114669028+AsteroidMe@users.noreply.github.com>
2024-12-13 19:54:11 +0700

    Add filter

--- repositories/artikel_repo.go
+++ repositories/artikel_repo.go
@@ -9,7 +9,7 @@ type ArtikelRepoInterface interface {
 	Create(artikel *entities.Artikel) (*entities.Artikel, error)
 	Update(artikel *entities.Artikel) (*entities.Artikel, error)
 	Delete(id int) error
-	FindAll(page int, limit int) ([]entities.Artikel, error)
+	FindAll(nama string, kategori string, page int, limit int) ([]entities.Artikel, error)
 	FindByID(id int) (*entities.Artikel, error)
 	GetAdminByID(id int) (*entities.Admin, error)
 	Count() (int64, error)
@@ -19,7 +19,7 @@ type artikelRepo struct {
 	db *gorm.DB
 }
 
-func NewArtikelRepo(db *gorm.DB) ArtikelRepoInterface {
+func NewArtikelRepo(db *gorm.DB) *artikelRepo {
 	return &artikelRepo{db: db}
 }
 
@@ -41,12 +41,25 @@ func (r *artikelRepo) Delete(id int) error {
 	return r.db.Delete(&entities.Artikel{}, id).Error
 }
 
-func (r *artikelRepo) FindAll(page int, limit int) ([]entities.Artikel, error) {
+func (r *artikelRepo) FindAll(nama string, kategori string, page int, limit int) ([]entities.Artikel, error) {
 	var artikels []entities.Artikel
 	offset := (page - 1) * limit
-	if err := r.db.Limit(limit).Offset(offset).Find(&artikels).Error; err != nil {
+
+	db := r.db.Model(&entities.Artikel{})
+
+	if nama != "" {
+		db = db.Where("nama LIKE ?", "%"+nama+"%")
+	}
+
+	if kategori != "" {
+		db = db.Where("kategori = ?", kategori)
+	}
+
+	err := db.Limit(limit).Offset(offset).Find(&artikels).Error
+	if err != nil {
 		return nil, err
 	}
+
 	return artikels, nil
 }
 
