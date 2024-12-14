9faa8cb027436ce1e4b4e80eaef8dea5b7471bec
Aldian Eka K <aldianeka88@gmail.com>
2024-12-14 10:01:51 +0700

    change json request to postform

--- controllers/adminProductControl.go
+++ controllers/adminProductControl.go
@@ -32,6 +32,18 @@ func (ac *AdminProductController) CreateAdminProduct(c *gin.Context) {
 		return
 	}
 
+	product.Gambar = filePath
+	product.Nama = c.PostForm("nama")
+	product.Deskripsi = c.PostForm("deskripsi")
+	product.Keunggulan = c.PostForm("keunggulan")
+	product.Harga, _ = strconv.ParseFloat(c.PostForm("harga"), 64)
+	product.Variasi = c.PostForm("variasi")
+	product.Kategori = c.PostForm("kategori")
+	product.KotaAsal = c.PostForm("kota_asal")
+	product.Stok, _ = strconv.Atoi(c.PostForm("stok"))
+	product.Status = c.PostForm("status")
+	product.TipsPenyimpanan = c.PostForm("tips_penyimpanan")
+
 	createdProduct, err := ac.service.CreateAdminProduct(&product)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
