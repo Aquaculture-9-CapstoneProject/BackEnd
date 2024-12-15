f1bb2d70fe780def2f52442817c74fb6b6f28001
Aldian Eka K <aldianeka88@gmail.com>
2024-12-15 09:22:29 +0700

    Clean func

--- repositories/artikelRepo.go
+++ repositories/artikelRepo.go
@@ -6,9 +6,6 @@ import (
 )
 
 type ArtikelRepoInterface interface {
-	Create(artikel *entities.Artikel) (*entities.Artikel, error)
-	Update(artikel *entities.Artikel) (*entities.Artikel, error)
-	Delete(id int) error
 	GetAll(page int, limit int) ([]entities.Artikel, error)
 	FindAll(judul string, kategori string, page int, limit int) ([]entities.Artikel, error)
 	FindByID(id int) (*entities.Artikel, error)
@@ -21,24 +18,6 @@ type artikelRepo struct {
 
 func NewArtikelRepo(db *gorm.DB) *artikelRepo {
 	return &artikelRepo{db: db}
-}
-
-func (r *artikelRepo) Create(artikel *entities.Artikel) (*entities.Artikel, error) {
-	if err := r.db.Create(artikel).Error; err != nil {
-		return nil, err
-	}
-	return artikel, nil
-}
-
-func (r *artikelRepo) Update(artikel *entities.Artikel) (*entities.Artikel, error) {
-	if err := r.db.Save(artikel).Error; err != nil {
-		return nil, err
-	}
-	return artikel, nil
-}
-
-func (r *artikelRepo) Delete(id int) error {
-	return r.db.Delete(&entities.Artikel{}, id).Error
 }
 
 func (r *artikelRepo) GetAll(page int, limit int) ([]entities.Artikel, error) {
