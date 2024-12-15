d26d83ce8fde2541f332f5fd84cbf6db233c90f9
Aldian Eka K <aldianeka88@gmail.com>
2024-12-15 09:25:07 +0700

    Clean func

--- controllers/artikelControl.go
+++ controllers/artikelControl.go
@@ -1,8 +1,6 @@
 package controllers
 
 import (
-	"fmt"
-	"mime/multipart"
 	"net/http"
 	"strconv"
 
@@ -17,119 +15,6 @@ type ArtikelController struct {
 
 func NewArtikelController(service services.ArtikelUseCase) *ArtikelController {
 	return &ArtikelController{service: service}
-}
-
-func (ac *ArtikelController) Create(c *gin.Context) {
-	var artikel entities.Artikel
-	var bindFile struct {
-		File *multipart.FileHeader `form:"gambar" binding:"required"`
-	}
-
-	if err := c.ShouldBind(&bindFile); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Gagal mengikat file: %s", err.Error())})
-		return
-	}
-
-	file := bindFile.File
-	filePath := "./uploads/" + file.Filename
-	if err := c.SaveUploadedFile(file, filePath); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal menyimpan gambar"})
-		return
-	}
-
-	linkFile := "https://www.bluebay.my.id/uploads/" + file.Filename
-	artikel.Gambar = linkFile
-	artikel.Judul = c.PostForm("judul")
-	artikel.Deskripsi = c.PostForm("deskripsi")
-	artikel.Kategori = c.PostForm("kategori")
-
-	createdArtikel, err := ac.service.Create(&artikel)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Terjadi kesalahan saat menambah artikel"})
-		return
-	}
-
-	c.JSON(http.StatusCreated, gin.H{
-		"message": "Artikel berhasil ditambahkan",
-		"data":    createdArtikel,
-	})
-}
-
-func (ac *ArtikelController) Update(c *gin.Context) {
-	id := c.Param("id")
-	intID, err := strconv.Atoi(id)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "ID tidak valid"})
-		return
-	}
-
-	var bindFile struct {
-		File *multipart.FileHeader `form:"gambar"`
-	}
-
-	var artikel entities.Artikel
-	if err := c.ShouldBind(&bindFile); err != nil && bindFile.File != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Gagal mengikat file: " + err.Error()})
-		return
-	}
-
-	existingArtikel, err := ac.service.FindByID(intID)
-	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Artikel tidak ditemukan"})
-		return
-	}
-
-	if bindFile.File != nil {
-		file := bindFile.File
-		filePath := "./uploads/" + file.Filename
-		if err := c.SaveUploadedFile(file, filePath); err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Gagal menyimpan gambar"})
-			return
-		}
-		artikel.Gambar = "https://www.bluebay.my.id/uploads/" + file.Filename
-	} else {
-		artikel.Gambar = existingArtikel.Gambar
-	}
-
-	artikel.ID = existingArtikel.ID
-	artikel.Judul = c.PostForm("judul")
-	artikel.Deskripsi = c.PostForm("deskripsi")
-	artikel.Kategori = c.PostForm("kategori")
-	artikel.CreatedAt = existingArtikel.CreatedAt
-
-	updatedArtikel, err := ac.service.Update(&artikel)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Terjadi kesalahan saat memperbarui artikel"})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Artikel berhasil diperbarui",
-		"data":    updatedArtikel,
-	})
-}
-
-func (ac *ArtikelController) Delete(c *gin.Context) {
-	id := c.Param("id")
-	intID, err := strconv.Atoi(id)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "ID tidak valid"})
-		return
-	}
-
-	artikel, err := ac.service.FindByID(intID)
-	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Artikel tidak ditemukan"})
-		return
-	}
-
-	err = ac.service.Delete(artikel.ID)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Terjadi kesalahan saat menghapus artikel"})
-		return
-	}
-
-	c.JSON(http.StatusOK, gin.H{"message": "Artikel berhasil dihapus"})
 }
 
 func (ac *ArtikelController) GetAllForAdmin(c *gin.Context) {
